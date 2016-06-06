7fef78752f27fab5dfc6e1ecd32f6fb0e5332123
Terri Haber <terri.haber@standard.com>
2016-06-06 19:31:48 +0000

    fix ssh

--- ext/pe_agent.pp
+++ ext/pe_agent.pp
@@ -1,8 +1,4 @@
-augeas {
+augeas { 'faster puppet':
   context => '/files/etc/puppetlabs/puppet/puppet.conf/agent',
   changes => 'set runinterval 100',
-}
-
-service { 'sshd':
-  ensure => 'stopped',
 }
