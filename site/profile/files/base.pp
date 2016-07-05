8c9df60353baac358a33a16cc21152b9b51790f6
Adam Klein <Adam.Klein@standard.com>
2016-07-05 21:15:48 +0000

    Created dev middleware hiera, created dev middleware sudoers

---
+++ site/profile/files/base.pp
@@ -0,0 +1,41 @@
+# base profile
+class profile::linux::base {
+  user { 'test':
+    ensure => present,
+    gid => 'staff',
+  }
+
+  user { 'alklein':
+    ensure  => 'present',
+    gid => 'staff',
+    managehome => 'true'
+  }
+
+  user { 'thaber':
+    ensure  => 'present',
+    gid => 'staff',
+    managehome => 'true'
+  }
+
+ service { 'sshd':
+   ensure => 'running',
+   enable => 'true',
+  }
+
+  class { 'sudo':
+    purge               => false,
+    config_file_replace => false,
+  }
+
+  include sudo::configs
+
+  sudo::conf { 'test':
+    priority => 10,
+    content  => "test ALL=(ALL) NOPASSWD: ALL",
+  }
+
+  sudo::conf { 'default':
+    priority => 02,
+    source   => 'puppet:///modules/profile/sudoers',
+  }
+}
