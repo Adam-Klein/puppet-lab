a3c58b97d55b132b7d92158226044979f0e4d9fb
Adam Klein <Adam.Klein@standard.com>
2016-07-28 11:31:46 -0700

    Updated deploy.sh script with Terri's changes to manage different branches

--- ext/deploy.sh
+++ ext/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# r10k deploy environment production -p
-r10k deploy environment Practice_1 -p
-rm -rf /etc/puppetlabs/code/environments/production && mv /etc/puppetlabs/code/environments/Practice_1 /etc/puppetlabs/code/environments/production
+BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD`
+
+r10k deploy environment $BRANCH -p
+
+if [ "$BRANCH" != "production" ];
+then
+  rm -rf /etc/puppetlabs/code/environments/production
+  mv /etc/puppetlabs/code/environments/${BRANCH} /etc/puppetlabs/code/environments/production
+fi
